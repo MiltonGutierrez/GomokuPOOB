972325fa6ca30532d7bf0dd64db8783597d5ce11
oscar0617 <oscarsantiagolesmes@gmail.com>
2023-12-16 16:09:31 -0500

    Miedosa

--- src/domain/FearfulMachine.java
+++ src/domain/FearfulMachine.java
@@ -37,82 +37,169 @@ public class FearfulMachine extends Machine {
     
     
 
-	@Override
-	public void setToken(Token token, int xPos, int yPos) {
-		// TODO Auto-generated method stub
-		
+    @Override
+    /**
+     * Gets the last token of gomoku
+     */
+	public void getTurn() {
+    	int[] ultimaFichaOponente = Gomoku.getGomoku().returnLastPositionToken();
+    	int dimensiones = Gomoku.getGomoku().getDimension();
+    	int xPosLast = ultimaFichaOponente[0];
+    	int yPosLast = ultimaFichaOponente[1];
+    	try {
+    		System.out.println("Voy a judar\n"+ xPosLast + "\n" + yPosLast);
+			play(xPosLast, yPosLast, dimensiones);
+		} catch (GomokuException e) {
+			Log.record(e);
+		}
 	}
+    
+    /**
+     * Plays placing tokens next to the last token of the player
+     * @param xPosLast x coordinate of the last token
+     * @param yPosLast y coordinate of the last token
+     * @param dimensiones is the dimension of the board
+     * @throws GomokuException
+     */
+    public void play(int xPosLast, int yPosLast, int dimensiones) throws GomokuException {
+    	if(!(xPosLast + 3 >= dimensiones) && Gomoku.getGomoku().validPlay(xPosLast + 3, yPosLast)) {
+    		Gomoku.getGomoku().play(xPosLast + 3, yPosLast);
+    	}else if(!(xPosLast - 3 < 0) && Gomoku.getGomoku().validPlay(xPosLast - 3, yPosLast)) {
+    		Gomoku.getGomoku().play(xPosLast - 3, yPosLast);
+    	}else if(!(yPosLast - 3 < 0) && Gomoku.getGomoku().validPlay(xPosLast, yPosLast - 3)){
+    		Gomoku.getGomoku().play(xPosLast, yPosLast-3);
+    	}else if(!(yPosLast + 3 >= dimensiones) && Gomoku.getGomoku().validPlay(xPosLast, yPosLast + 3)) {
+    		Gomoku.getGomoku().play(xPosLast, yPosLast+3);
+    	}
+    }
+
 
 	@Override
-	public void startTime() {
-		// TODO Auto-generated method stub
-		
-	}
+	/**
+     * Starts the time the player takes to play
+     */
+    public void startTime(){
+    	for(Timer timer: timers.values()) {
+            if (!timer.isRunning()) {
+                timer.start();
+            }
+    	}
+
+    }
 
 	@Override
-	public void stopTime() {
-		// TODO Auto-generated method stub
-		
-	}
+	/**
+     * Stops the time the player takes to play
+     */
+    public void stopTime() {
+    	for(Timer timer: timers.values()) {
+            if (timer.isRunning()) {
+                timer.stop();
+            }
+    	}
+
+    }
 
 	@Override
+	/**
+	 * Returns the time that has passed between turns
+	 */
 	public TimePassed getTime(String time) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.times.get(time);
 	}
 
 	@Override
-	public void setTokenMatrix(Token[][] matrix) {
-		// TODO Auto-generated method stub
-		
-	}
+	/**
+     * Sets the token matrix for the player.
+     * @param matrix: Token matrix
+     */
+    public void setTokenMatrix(Token[][] matrix){
+        this.tokenMatrix = matrix;
+    }
 
 	@Override
-	public Token[][] returnTokenMatrix() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	/**
+     * Returns the token matrix of the player
+     */
+    public Token[][] returnTokenMatrix(){
+        return tokenMatrix;
+    }
+    
 
 	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	/**
+     * Return the name of the player.
+     */
+    public String getName(){
+        return name;
+    }
 
 	@Override
-	public void deleteToken(int xPos, int yPos) {
-		// TODO Auto-generated method stub
-		
-	}
+	/**
+     * Deletes the token in the tokenMatrix and the arrayList tokens
+     * @param xPos x position to delete
+     * @param yPos y position to delete
+     */
+    public void deleteToken(int xPos, int yPos){
+    	this.tokens.remove(this.tokenMatrix[xPos][yPos]);
+        tokenMatrix[xPos][yPos] = null;
+    }
 
 	@Override
+	/**
+	 * Sets the arraylist containing string that determine the tokens the player's going to use.
+	 * @param tokens are the tokens of the player
+	 */
 	public void setTokensToUse(ArrayList<String> tokens) {
-		// TODO Auto-generated method stub
-		
+		tokensToUse = tokens;
 	}
 
 	@Override
+	/**
+	 * Returns the string containing the name of the token subclass the player's going to use.
+	 */
 	public String getTokenToUse() {
-		// TODO Auto-generated method stub
-		return null;
+		String gameMode = Gomoku.getGomoku().getGameMode();
+		String tokenType = tokensToUse.get(0);
+		if(tokensToUse.size() > 0 ){
+			tokensToUse.remove(0);
+		}
+		else if(gameMode.equals("normal") || gameMode.equals("quicktime") && tokensToUse.size() == 0) {
+			try {
+				gomoku.createTokensToUse(this.name);
+			} catch (GomokuException e) {
+				Log.record(e);
+			} // evitamos que se quede sin fichas.
+		}
+		else if(tokensToUse.size() == 0 && gameMode.equals("limited")) {
+			return null;
+		}
+		return tokenType;
 	}
 
 	@Override
+	/**
+	 * Sets the time left
+	 */
 	public void setTime(Integer time) {
-		// TODO Auto-generated method stub
+		times.get("timeM").setTime(time);
 		
 	}
 
 	@Override
+	/**
+	 * Validates the time left
+	 */
 	public boolean validateTime() {
-		// TODO Auto-generated method stub
-		return false;
+		return times.get("timeM").getTime() <= 0;
 	}
 
 	@Override
+	/**
+	 * Returns the tokens left to use by the player.
+	 */
 	public int getTokensLeft() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.tokensToUse.size();
 	}
 
 	@Override
@@ -138,28 +225,41 @@ public class FearfulMachine extends Machine {
 		return color;
 	}
 
+	/**
+	 * Adds a token to the player
+	 * @param tokenType is the type of token to add
+	 */
 	@Override
 	public void addTokenToUse(String tokenType) {
-		// TODO Auto-generated method stub
-		
+		this.tokensToUse.add(0, tokenType);
 	}
 
 	@Override
+	/**
+	 * Adds the score
+	 * @Score is the score to add or substract to the player
+	 */
 	public void setPuntuacion(int score_) {
-		// TODO Auto-generated method stub
-		
+		int sumar = score_;
+		this.score += sumar;
 	}
+	
 
 	@Override
+	/**
+	 * Returns the current score of the player
+	 */
 	public int getPuntuacion() {
-		// TODO Auto-generated method stub
-		return 0;
+		return score;
 	}
 
 	@Override
+	/**
+	 * Returns the tokens left to use
+	 * @return
+	 */
 	public int tokensLeft() {
-		// TODO Auto-generated method stub
-		return 0;
+		return tokensToUse.size();
 	}
 
 	@Override
@@ -168,13 +268,16 @@ public class FearfulMachine extends Machine {
 		
 	}
 
-
-
 	@Override
-	public void getTurn() {
-		// TODO Auto-generated method stub
-		
-	}
+	/**
+     * Sets the token in the respective lists and matrixes
+     */
+    public void setToken(Token token, int xPos, int yPos){
+        tokens.add(token);
+        tokenMatrix[xPos][yPos] = token;
+    }
+
+	
     
 
 
